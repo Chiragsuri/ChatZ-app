14c469dad0b6b145bf6e8a8146dfe82531585af8
unknown <deepakpalrocks@gmail.com>
2023-04-23 22:02:27 +0530

    updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 
 const connectDB = async ()=>{
+  dotenv.config();
   console.log(process.env.MONGO_URI);
     try {
+      console.log("URI",process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
@@ -11,7 +14,7 @@ const connectDB = async ()=>{
     
         console.log(`MongoDB Connected: ${conn.connection.host}`); 
       } catch(err){
-        console.log(`Error: ${error.message}`);
+        console.log(`Error: ${err.message}`);
         process.exit();
       }
 };
