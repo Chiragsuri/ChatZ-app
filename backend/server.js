1279ff5f8b75a26cf915c5b2dddbc49e174340b3
Deepak Pal <85776383+deepakpalrocks@users.noreply.github.com>
2023-05-11 15:48:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,14 @@ app.use(express.json());
 app.use('/api/user', userRoutes); 
 app.use('/api/chat', chatRoutes);     
 app.use("/api/message", messageRoutes);
-
+// CORS Fix
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-FCM-Token, X-Device-ID ,Content-Type, Accept, Authorization, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Refresh-Authorization");
+  next();
+});
 
 // --------------------------deployment------------------------------
 
